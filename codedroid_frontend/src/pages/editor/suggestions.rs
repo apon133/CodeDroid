166208b9ab39a1ac86b699f99875fb054d860d09
gokeihub <mdaponahmed412@gmail.com>
2026-05-30 14:40:02 +0600

    refactor: consolidate suggestion selection handling into a single click event listener

--- codedroid_frontend/src/pages/editor/suggestions.rs
+++ codedroid_frontend/src/pages/editor/suggestions.rs
@@ -24,14 +24,11 @@ pub fn SuggestionsOverlay(
                     >
                         {move || suggestions.get().into_iter().enumerate().map(|(i, s)| {
                             let s2 = s.clone();
-                            let s3 = s.clone();
                             view! {
                                 <button 
                                     class=move || if selected_idx.get() == i { "suggestion-item selected" } else { "suggestion-item" }
                                     on:mousedown=move |e: web_sys::MouseEvent| { e.prevent_default(); }
-                                    on:mouseup=move |e: web_sys::MouseEvent| { e.prevent_default(); on_select.run(s2.clone()); }
-                                    on:click=move |e: web_sys::MouseEvent| { e.prevent_default(); }
-                                    on:touchstart=move |e: web_sys::TouchEvent| { e.prevent_default(); on_select.run(s3.clone()); }
+                                    on:click=move |e: web_sys::MouseEvent| { e.prevent_default(); on_select.run(s2.clone()); }
                                     on:mouseenter=move |_| selected_idx.set(i)
                                 >
                                     <span class="suggestion-kind">{kind_icon(s.kind)}</span>
